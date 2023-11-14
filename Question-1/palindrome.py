958407a9effc9e288b20dd44c84d991612d36df3
NandiniSrinivas <nandininandy817@gmail.com>
2023-11-14 22:40:29 +0530

    adding palindrone file

---
+++ Question-1/palindrome.py
@@ -0,0 +1,14 @@
+def is_palindrome(s):
+    # Remove spaces and convert to lowercase for case-insensitive comparison
+    s = s.replace(" ", "").lower()
+
+    # Compare the original string with its reverse
+    return s == s[::-1]
+
+
+# Example usage
+word = input("Enter a word or phrase: ")
+if is_palindrome(word):
+    print(f"{word} is a palindrome.")
+else:
+    print(f"{word} is not a palindrome.")
